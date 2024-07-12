871bf2129346a6c5593cefc0ce0976ee8e462d40
fomes <fgomesdeluna@gmail.com>
2024-07-12 14:36:14 -0300

    feat: include footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Footer from "@/components/footer/footer";
 import Hero from "@/components/hero/hero";
+import Navbar from "@/components/navbar/navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
---
+++ components/footer/footer.tsx
@@ -0,0 +1,106 @@
+import Link from "next/link";
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+
+const CURRENT_YEAR = new Date().getFullYear();
+const iconClass = "hover:scale-125 transition duration-200";
+
+const LINKS = [
+  {
+    icon: <Linkedin className={iconClass} />,
+    href: "https://www.linkedin.com/in/company/pmiceara",
+  },
+  {
+    icon: <Instagram className={iconClass} />,
+    href: "https://www.instagram.com/pmiceara/",
+  },
+  {
+    icon: <Twitter className={iconClass} />,
+    href: "https://twitter.com/pmiceara",
+  },
+  {
+    icon: <Youtube className={iconClass} />,
+    href: "https://www.youtube.com/channel/UCQ3OoimV8zxsGgm1vH0Sfpw",
+  },
+  {
+    icon: <Facebook className={iconClass} />,
+    href: "https://www.facebook.com/pmiceara",
+  },
+];
+
+export function Footer() {
+  return (
+    <footer id="footer" className="pb-5 p-10 md:pt-10">
+      <div className="container flex flex-col mx-auto items-center text-white">
+        <div className="flex !w-full py-10 mb-5 md:mb-20 flex-col justify-center items-center bg-gray-900 text-slate-100 max-w-6xl mx-auto rounded-2xl p-5 ">
+          <p
+            className="text-2xl md:text-3xl text-center font-bold "
+            color="white"
+          >
+            Garanta seu ingresso e junte-se a nós!
+          </p>
+          <p color="white" className=" md:w-7/12 text-center my-3 !text-base">
+            Não perca esta oferta exclusiva que terminará em breve.
+          </p>
+          <div className="flex w-fit gap-3 mt-2 flex-col md:flex-row">
+            <Link
+              target="_blank"
+              className="w-fit"
+              href={
+                "https://www.sympla.com.br/evento/14-congresso-regional-de-gestao-projetos-e-lideranca-pmice/2441790"
+              }
+            >
+              <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+                Comprar
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center !justify-between">
+          <ul className="flex justify-center my-4 md:my-0 w-max mx-auto items-center gap-4">
+            {LINKS.map((link, index) => (
+              <li key={index}>
+                <Link
+                  href={link.href}
+                  className="font-normal !text-gray-700 hover:!text-gray-900 transition-colors"
+                >
+                  {link.icon}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <p
+            color="blue-gray"
+            className="text-center text-xs mt-12 font-normal !text-gray-700"
+          >
+            &copy; {CURRENT_YEAR} Made with{" "}
+            <a href="https://nextjs.org/" target="_blank" className="font-bold">
+              Next{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://www.material-tailwind.com/"
+              target="_blank"
+              className="font-bold"
+            >
+              Material Tailwind{" "}
+            </a>
+            by{" "}
+            <a
+              href="https://fgomes.vercel.app/"
+              target="_blank"
+              className="font-bold"
+            >
+              Felipe Gomes
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
