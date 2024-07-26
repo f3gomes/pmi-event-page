5a763f7a0530e9163b62c10f87085fb9d3cd5186
fomes <fgomesdeluna@gmail.com>
2024-07-26 10:53:04 -0300

    feat: include card fade animation

---
+++ utils/animations.ts
@@ -0,0 +1,11 @@
+export const fadeUpScale = {
+  initial: { opacity: 0, scale: 0 },
+  whileInView: { opacity: 1, scale: 1 },
+  exit: { opacity: 0, scale: 0 },
+};
+
+export const fadeUp = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 },
+};
