c1a49cd3f1b064f108d22e6e5f659920bce62bcd
fomes <fgomesdeluna@gmail.com>
2024-07-26 18:11:18 -0300

    feat: include about footer

---
+++ components/buy-button/index.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+interface BuyButtonProps {
+  home: boolean;
+  showButton: boolean;
+  purchaseLink: string;
+}
+
+const BuyButton = ({ home, showButton, purchaseLink }: BuyButtonProps) => {
+  return (
+    <Link
+      target="_blank"
+      href={purchaseLink}
+      className={cn(
+        showButton || !home
+          ? "hidden xl:block xl:right-6"
+          : "hidden xl:right-6",
+        "absolute"
+      )}
+    >
+      <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-100 focus:outline-none bg-event-200 hover:brightness-125 hover:text-gray-200 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 transition duration-200">
+        Comprar
+      </button>
+    </Link>
+  );
+};
+
+export default BuyButton;
