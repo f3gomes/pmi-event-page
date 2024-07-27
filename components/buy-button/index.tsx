e4063d7c401433f54c5978d3852cc8de5dde5443
fomes <fgomesdeluna@gmail.com>
2024-07-26 21:58:01 -0300

    fix: ajust navbar padding

--- components/buy-button/index.tsx
+++ components/buy-button/index.tsx
@@ -13,10 +13,8 @@ const BuyButton = ({ home, showButton, purchaseLink }: BuyButtonProps) => {
       target="_blank"
       href={purchaseLink}
       className={cn(
-        showButton || !home
-          ? "hidden xl:block xl:right-6"
-          : "hidden xl:right-6",
-        "absolute"
+        showButton || !home ? "hidden xl:block" : "hidden",
+        "w-[6.6rem]"
       )}
     >
       <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-100 focus:outline-none bg-event-200 hover:brightness-125 hover:text-gray-200 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 transition duration-200">
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -67,6 +67,33 @@ const NAV_MENU = [
     name: "Local",
     href: "#location",
   },
+];
+
+const NAV_MENU_MOBILE = [
+  {
+    name: "Início",
+    href: "#",
+  },
+  {
+    name: "Sobre",
+    href: "#about",
+  },
+  {
+    name: "Speakers",
+    href: "#speakers",
+  },
+  {
+    name: "Patrocínio",
+    href: "#sponsor",
+  },
+  {
+    name: "Data",
+    href: "#timer",
+  },
+  {
+    name: "Local",
+    href: "#location",
+  },
   {
     name: "Comprar",
     href: purchaseLink,
@@ -125,10 +152,10 @@ const Navbar = ({ home }: NavbarProps) => {
       fullWidth
       blurred={false}
       color={isScrolling ? "white" : "transparent"}
-      className="fixed top-0 z-50 border-0 h-28"
+      className="fixed top-0 z-50 border-0 h-28 p-0"
     >
-      <div className="container mx-auto h-full flex items-center justify-center">
-        <Link href={"/"} className="absolute top-0 left-0">
+      <div className="h-full flex items-center justify-between px-8 lg:px-16 xl:px-20">
+        <Link href={"/"}>
           <Image
             priority
             width={0}
@@ -136,11 +163,11 @@ const Navbar = ({ home }: NavbarProps) => {
             sizes="100vw"
             src={"/logo/logo.png"}
             alt="logo"
-            className="cursor-pointer w-auto h-28"
+            className="cursor-pointer w-auto h-20"
           />
         </Link>
 
-        <div className="">
+        <div className="absolute left-1/2 -ml-[225px]">
           <ul
             className={` hidden items-center gap-6 xl:flex ${isScrolling || !home ? "text-gray-900" : "text-white"
               }`}
@@ -163,7 +190,7 @@ const Navbar = ({ home }: NavbarProps) => {
           variant="text"
           color={"gray"}
           onClick={handleOpen}
-          className="ml-auto inline-block xl:hidden"
+          className="inline-block xl:hidden"
         >
           {open ? (
             <XMarkIcon strokeWidth={2} className="h-6 w-6" />
@@ -176,10 +203,10 @@ const Navbar = ({ home }: NavbarProps) => {
       <Collapse open={open}>
         <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
           <ul className="flex flex-col gap-4 text-gray-900">
-            {NAV_MENU.map(({ name, href }) => (
-              <>
+            {NAV_MENU_MOBILE.map(({ name, href }) => (
+              <div key={name}>
                 {name === "Comprar" ? (
-                  <Link target="_blank" href={purchaseLink}>
+                  <Link key={name} target="_blank" href={purchaseLink}>
                     <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-100 focus:outline-none bg-event-200 hover:brightness-125 hover:text-gray-200 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 transition duration-200">
                       Comprar
                     </button>
@@ -194,7 +221,7 @@ const Navbar = ({ home }: NavbarProps) => {
                     {name}
                   </NavItem>
                 )}
-              </>
+              </div>
             ))}
           </ul>
         </div>
