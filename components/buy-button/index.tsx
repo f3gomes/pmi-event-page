e88ca134efc3d7e51b74078f213052291f853d4f
fomes <fgomesdeluna@gmail.com>
2024-07-30 16:39:17 -0300

    fix: buy btn width

--- components/buy-button/index.tsx
+++ components/buy-button/index.tsx
@@ -14,7 +14,7 @@ const BuyButton = ({ home, showButton, purchaseLink }: BuyButtonProps) => {
       href={purchaseLink}
       className={cn(
         showButton || !home ? "hidden xl:block" : "hidden",
-        "py-2.5 px-5 mb-2 text-sm font-medium text-gray-100 focus:outline-none bg-event-200 hover:brightness-125 hover:text-gray-200 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 transition duration-200 flex justify-center items-center"
+        "w-[6.6rem] py-2.5 px-5 mb-2 text-sm font-medium text-gray-100 focus:outline-none bg-event-200 hover:brightness-125 hover:text-gray-200 rounded-lg border border-gray-200 focus:z-10 focus:ring-4 focus:ring-gray-100 transition duration-200"
       )}
     >
       Comprar
